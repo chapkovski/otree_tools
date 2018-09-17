fb1b767df5854455625961188d94aaa12616923b
chapkovski <chapkovski@gmail.com>
2018-09-17 14:04:51 +0300

    before adding vars export

--- otree_tools/templatetags/otree_tools.py
+++ otree_tools/templatetags/otree_tools.py
@@ -38,7 +38,7 @@ def button(context, label='', *args, **kwargs):
     for kwarg in kwargs:
         raise ButtonTagError(
             # need double {{ to escape because of .format()
-            '{{% chat %}} tag received unrecognized parameter "{}"'.format(kwarg)
+            '{{% button %}} tag received unrecognized parameter "{}"'.format(kwarg)
         )
     context['label'] = label
     return context
