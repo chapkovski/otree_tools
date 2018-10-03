26c6afa9fbccd1bf5ff0c2aec56f194be423bc65
chapkovski <chapkovski@gmail.com>
2018-10-03 14:43:57 +0300

    before inserting json from sep app

--- otree_tools/otree_extensions/urls.py
+++ otree_tools/otree_extensions/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url, include
 
 from otree_tools import views as v
 
-
 urlpatterns = [url(v.ListPVarsView.url_pattern, v.ListPVarsView.as_view(),
                    name=v.ListPVarsView.url_name),
                url(v.PVarsCSVExport.url_pattern, v.PVarsCSVExport.as_view(),
                    name=v.PVarsCSVExport.url_name),
+               url(r'^session_data/(?P<session_code>.*)/(?P<filetype>.*)/$', v.SpecificSessionDataView.as_view(),
+                   name='session_data'),
                ]
