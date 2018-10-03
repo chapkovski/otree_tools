cc2e8744b79c1edb7c69371db10a45d3b08c1f65
chapkovski <chapkovski@gmail.com>
2018-10-03 15:53:37 +0300

    minor changes in admin

--- otree_tools/otree_extensions/admin.py
+++ otree_tools/otree_extensions/admin.py
@@ -1,6 +1,6 @@
 # TODO: incorporate ~admin to include mturk, paypal and json export
 
 
-from otree_tools.views import EnterExitEventList, FocusEventList, PVarsSessionsList, AllSessionsList
-data_export_views = [EnterExitEventList, FocusEventList,PVarsSessionsList, AllSessionsList]
-# TODO: export participant vars export
+from otree_tools.views import EnterExitEventList, FocusEventList, AllSessionsList
+
+data_export_views = [AllSessionsList, EnterExitEventList, FocusEventList, ]
