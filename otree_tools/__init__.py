5e60908b9f5dd6864dbea423edb017ac799d9468
chapkovski <chapkovski@gmail.com>
2019-03-23 13:03:15 +0100

    before adding control for browser compatibility

--- otree_tools/__init__.py
+++ otree_tools/__init__.py
@@ -1,2 +1,13 @@
 __version__ = '1.0.2'
 
+"""The following is needed just for naive debugging."""
+from termcolor import colored
+import shutil
+
+
+def cp(*x, color='red', center=True):
+    columns = shutil.get_terminal_size().columns
+    x = ' '.join([str(i) for i in x])
+    if center:
+        x = x.center(columns, '-')
+    print(colored(x, color, 'on_cyan', attrs=['bold']))
